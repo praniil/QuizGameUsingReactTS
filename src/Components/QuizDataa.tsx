5840362f83b97d0b9ba22bba4fe63c39cd8f1092
praniil <pparajuli15@gmail.com>
2023-10-23 13:46:29 +0545

    completed the quiz game app

---
+++ src/Components/QuizDataa.tsx
@@ -0,0 +1,37 @@
+export const QuizData = [
+    {
+        question : "In which district is Fungfung waterfal?",
+        options : ["Kathmandu", "Tanahun", "Nuwakot", "Pokhara"],
+        answer : 3
+    },
+    {
+        question : "How much of the Heritage of Nepal is to be celebrated in the World Heritage Site?",
+        options : [10, 11, 5, 9],
+        answer : 1
+    },
+    {
+        question : "Which is the first magazine in the Nepali language?",
+        options : ["gorkha patra", "gorkha india life", "kastamandap magazine", "nepal magazine"],
+        answer : 2
+    },
+    {
+        question : "Which is the first highway of Nepal?",
+        options : ["Prithvi Highway", "Tribhuvan Rajpath", "East west highesy", "None"],
+        answer : 2
+    },
+    {
+        question : " Who was the Zero inventor?",
+        options : ["Albert Eienstien", "Issac Newton", "Alan Turing", "Arya Bhatt"],
+        answer : 4
+    },
+    {
+        question : "Who is the father of Computer science?",
+        options : ["Charles Babbage", "Alan Turing", "Issac Newton", "None"],
+        answer : 2
+    },
+    {
+        question : "In the context of computer science, CAD stands for?",
+        options : ["Computer Aided Design", "Computer Aided Development", "Computer Advanced Design", "None"],
+        answer : 1
+    }
+]
---
+++ src/Components/QuizUI.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { QuizData } from "./QuizDataa";
+
+const QuizUI = () => {
+  const [number, setNumber] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showTotalScore, setShowTotalScore] = useState<boolean>(false);
+  const [answerVar, setAanswerVar] = useState(-1);
+
+  function handleClick() {
+    if (currentQuestion === QuizData.length - 1) {
+      //   setCurrentQuestion(0);
+      //   setNumber(1);
+      setShowTotalScore(true);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+      setNumber(number + 1);
+    }
+  }
+  function handleCheckAnswer(selectedAnswer: number) {
+    console.log(selectedAnswer);
+    console.log(QuizData[currentQuestion].answer);
+
+    if (selectedAnswer === QuizData[currentQuestion].answer) {
+      setScore(score + 1)
+    }
+    handleClick()
+  }
+
+  function handleStartAgain() {
+    setCurrentQuestion(0);
+    setNumber(1);
+    setShowTotalScore(false);
+  }
+
+  function SetVar(index: number) {
+    console.log("inside set var", index)
+    setAanswerVar(index)
+    
+  }
+
+  return (
+    <div className="w-96 h-auto m-auto">
+      <div className="flex justify-center">
+        <span className="flex"> {number}. &nbsp; &nbsp; &nbsp; </span>
+        <span className="flex justify-center w-full">
+          {QuizData[currentQuestion].question}
+        </span>
+      </div>
+      <div className="flex flex-col">
+        {QuizData[currentQuestion].options.map((option, index) => (
+          <button
+            key={index}
+            onClick={() => SetVar(index+1)}
+            className=" text-red-900 rounded hover:rounded-lg bg-red-300 m-2 p-2"
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+      <div className="flex justify-center m-3 cursor-pointer">
+        {showTotalScore ? (
+          <div>
+            {" "}
+            Total Score : {score} <br />{" "}
+            <button
+              onClick={handleStartAgain}
+              className="p-1 text-blue-700 bg-blue-400 rounded"
+            >
+              Start Again
+            </button>
+          </div>
+        ) : (
+          <div
+            className="flex justify-center p-1 text-blue-700 bg-blue-400 rounded w-14"
+            onClick={()=> handleCheckAnswer(answerVar) }
+          >
+            Next
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default QuizUI;
